b2a027cc130779c388e870288f284209cb54ef27
Dhilipan180405 <dhilipan182005@gmail.com>
2026-03-25 08:37:08 +0530

    first commit

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ BRONZE_CHICAGO_PATH = "bronze/chicago_api/"
 CHECKPOINT_FILE = "ingestion/chicago/checkpoint.txt"
 
 # -------- YOUTUBE --------
-YOUTUBE_API_KEY = "AIzaSyCMF61ChAhsh_K9DkZWuAsXX0gprQpidl8"
+YOUTUBE_API_KEY = "YOUR_API_KEY"
 YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3"
 BRONZE_YOUTUBE_PATH = "bronze/youtube/"
