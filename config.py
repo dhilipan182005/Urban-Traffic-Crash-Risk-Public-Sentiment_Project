de67c118717bf7cd418153b7f9eb2ebbacda32e9
Dhilipan180405 <dhilipan182005@gmail.com>
2026-03-25 08:25:39 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+# -------- CHICAGO --------
+CHICAGO_API_URL = "https://data.cityofchicago.org/resource/85ca-t3if.json"
+LIMIT = 1000
+SLEEP_TIME = 1
+MAX_RETRY = 5
+
+BRONZE_CHICAGO_PATH = "bronze/chicago_api/"
+CHECKPOINT_FILE = "ingestion/chicago/checkpoint.txt"
+
+# -------- YOUTUBE --------
+YOUTUBE_API_KEY = "AIzaSyCMF61ChAhsh_K9DkZWuAsXX0gprQpidl8"
+YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3"
+BRONZE_YOUTUBE_PATH = "bronze/youtube/"
