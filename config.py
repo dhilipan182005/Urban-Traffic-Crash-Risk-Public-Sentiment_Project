afc84a46c349b95b9fb09e6520ff03c51ea12625
Dhilipan180405 <dhilipan182005@gmail.com>
2026-03-25 13:39:25 +0530

    first commit

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+
+if not YOUTUBE_API_KEY:
+    raise ValueError("Missing API key")
 
 # -------- CHICAGO --------
 CHICAGO_API_URL = "https://data.cityofchicago.org/resource/85ca-t3if.json"
@@ -10,6 +18,6 @@ BRONZE_CHICAGO_PATH = "bronze/chicago_api/"
 CHECKPOINT_FILE = "ingestion/chicago/checkpoint.txt"
 
 # -------- YOUTUBE --------
-YOUTUBE_API_KEY = "YOUR_API_KEY"
+
 YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3"
 BRONZE_YOUTUBE_PATH = "bronze/youtube/"
